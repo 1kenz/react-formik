002afa445051ab4258b63a1d5eb51a1f632181c0
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-02 22:25:51 +0300

    feat: update component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import MyForm from "./components/MyForm";
+import MyFormYup from "./components/MyFormYup";
 
 function App() {
   return (
     <div className="App">
-      <MyForm />
+      <MyFormYup />
     </div>
   );
 }
